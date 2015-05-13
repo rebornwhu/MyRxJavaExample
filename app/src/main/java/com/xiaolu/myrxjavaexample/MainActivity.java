e0ebed749106cb0310d46b19488ca383f188b9f3
rebornwhu <rebornwhu@gmail.com>
2015-05-12 20:00:32 -0700

    Shorten the implementation by using anonymous inner class

--- app/src/main/java/com/xiaolu/myrxjavaexample/MainActivity.java
+++ app/src/main/java/com/xiaolu/myrxjavaexample/MainActivity.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import rx.Observable;
-import rx.Subscriber;
 import rx.functions.Action1;
 import rx.functions.Func1;
 
@@ -13,11 +15,16 @@ import rx.functions.Func1;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
+    private ArrayList<String> urls = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /**
+         * First article
+         */
         Observable.just("Hello, world!")
                 // Func1<T1, R> T1 represents the input type, R represents the return type
                 // here T1 is String - "Hello Word", R - Integer
@@ -28,11 +35,63 @@ public class MainActivity extends AppCompatActivity {
                         return s.hashCode();
                     }
                 })
-                .subscribe(new Action1<Integer>() {
+                .map(new Func1<Integer, String>() {
                     @Override
-                    public void call(Integer s) {
-                        Log.w(TAG, Integer.toString(s));
+                    public String call(Integer i) {
+                        return i.toString();
+                    }
+                })
+                // When susbscribe, we can see the return type is String
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String s) {
+                        Log.w(TAG, s);
                     }
                 });
+
+        /**
+         * Second article
+         */
+        urls.add("url1");
+        urls.add("url2");
+        urls.add("url3");
+
+        query("Hello, world!")
+                .flatMap(new Func1<List<String>, Observable<String>>() {
+                    @Override
+                    public Observable<String> call(List<String> urls) {
+                        return Observable.from(urls);
+                    }
+                })
+                .flatMap(new Func1<String, Observable<String>>() {
+                    @Override
+                    public Observable<String> call(String url) {
+                        return getTitle(url);
+                    }
+                })
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String s) {
+                        Log.w(TAG, s);
+                    }
+                });
+    }
+
+    // This method mimic an API call
+    Observable<ArrayList<String>> query(String s) {
+        // just() can create an Observable from any object AS IT IS
+        return Observable.just(urls);
+    }
+
+    // This method mimic an API call
+    Observable<String> getTitle(String url) {
+        String result;
+        if (url.equals("url3"))
+            result = "Do or die";
+        else if (url.equals("url1"))
+            result = "2 Fast 2 Furious";
+        else
+            result = "Fast 7";
+        return Observable.just(result);
     }
 }
